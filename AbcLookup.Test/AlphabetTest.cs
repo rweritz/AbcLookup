b33ea21c4f48e8955fab1d82433c10d7c7d759f6
Rüdiger Weritz <16781187+rweritz@users.noreply.github.com>
2020-08-17 20:03:19 +0200

    init

---
+++ AbcLookup.Test/AlphabetTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace AbcLookup.Test
+{
+    public class AlphabetTest
+    {
+        [Fact]
+        public void GetNext_HappyCases()
+        {
+            Assert.Equal('B', Alphabet.GetNext('A'));
+            Assert.Equal('D', Alphabet.GetNext('C'));
+        }
+        
+        [Fact]
+        public void GetPrevious_HappyCases()
+        {
+            Assert.Equal('A', Alphabet.GetPrevious('B'));
+            Assert.Equal('Y', Alphabet.GetPrevious('Z'));
+        }
+    }
+}
---
+++ AbcLookup/Alphabet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbcLookup
+{
+    public class Alphabet
+    {
+        private static readonly List<char> AbcList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().ToList();
+
+        public static char GetNext(char input)
+        {
+            var i = AbcList.IndexOf(input) + 1;
+            return AbcList[i];
+        }
+        
+        public static char GetPrevious(char input)
+        {
+            var i = AbcList.IndexOf(input) - 1;
+            return AbcList[i];
+        }
+        
+    }
+}
